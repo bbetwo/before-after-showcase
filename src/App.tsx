692be1d0f8e115a028c38ef51f59ea2a8c4399d8
bbe_two <krvrk.krmst@gmail.com>
2025-09-04 15:39:11 +0300

    style: update header, add beta badge

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import { CompareImage } from "./components/CompareImage";
 function App() {
     return (
         <div className="flex flex-col justify-center items-center">
-            <div className="bg-amber-600 p-1.5 mb-2.5">Портфолио </div>
+            <div className="relative text-4xl font-light text-gray-700 mb-8 border-b-2 border-red-600 pb-2">
+                Портфолио
+                <span className="absolute -top-[-37px] -right-8 bg-red-600 text-white text-xs font-bold px-2 py-1 rounded-sm">
+                    Beta v0.1
+                </span>
+            </div>
             <CompareImage />
         </div>
     );
